fc1adf1b02d212e227feac9e3f2e73f6a53c4957
willowjordan <willowtjordan@gmail.com>
2025-12-08 11:29:35 -0500

    first commit

---
+++ Day 1/decoder.py
@@ -0,0 +1,26 @@
+print("Hello world")
+
+# Open the file in read mode
+file = open("input.txt", "r")
+
+# Read line by line, decoding combination
+currentPos = 50 # position of dial
+zeroCount = 0 # number of times the dial has landed on 0
+for line in file:
+    amount = int(line[1:]) # how far to turn dial
+    print(f"{line} - turn {amount} times")
+    if line[0] == "R":
+        currentPos += amount
+    elif line[0] == "L":
+        currentPos -= amount
+    else:
+        print("Warning: unrecognized direction")
+    currentPos = currentPos % 100
+    print(f"Current position: {currentPos}")
+    if currentPos == 0:
+        zeroCount += 1
+
+print(f"The dial landed on zero exactly {zeroCount} times")
+
+# Close the file
+file.close()
